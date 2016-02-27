9d6e1bbfba097916c9af922debcc9a8f77eeefca
Jake Cooper <jetdude27@gmail.com>
2016-02-27 14:34:20 +0000

    Add IP Logging

--- cakeclub/js/controllers.js
+++ cakeclub/js/controllers.js
@@ -1,6 +1,6 @@
 var cakeclubApp = angular.module('cakeclubApp', ["firebase"]);
 
-cakeclubApp.controller('MainController',  function ($scope, $http, $firebaseObject, $firebaseArray) {
+cakeclubApp.controller('MainController',  function ($scope, $http, $firebaseObject, $firebaseArray, $window) {
     
   var ref = new Firebase("https://luminous-inferno-3515.firebaseio.com/" + "currentClubber");
   var syncObject = $firebaseObject(ref);
@@ -16,12 +16,26 @@ cakeclubApp.controller('MainController',  function ($scope, $http, $firebaseObje
     };
     
   $scope.nextClubber = function() {
-    if($scope.currentClubber != null) {
-      $scope.cakeclubbers.$add($scope.currentClubber);
-    }
-    var nextClubber = $scope.cakeclubbers[0];
-    $scope.cakeclubbers.$remove(nextClubber);
-    $scope.currentClubber = nextClubber;
+      if($window.confirm("Are you sure this person has baked, there will be DIRE consequences if you are lying.... Remember I have your IP:"+$scope.userip)) {
+        if($scope.currentClubber != null) {
+          $scope.cakeclubbers.$add($scope.currentClubber);
+        }
+        var nextClubber = $scope.cakeclubbers[0];
+        $scope.cakeclubbers.$remove(nextClubber);
+        $scope.currentClubber = nextClubber;
+      } else {
+        $window.alert("I appreciate your honesty ")
+      }
   };
-  
+
+    $scope.userip = 'Unknown';
+
+    var getIP = function() {
+        $http.get("https://api.ipify.org/")
+            .success(function (response) {
+                $scope.userip = response;
+            })
+    }
+
+    getIP();
 });
